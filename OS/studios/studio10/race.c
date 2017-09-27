506cf2196f691c6fa1dfdc52cc7d67fb66011c47
gannonmg <gannonm3211@gmail.com>
2017-09-27 14:21:43 -0500

    studio 10 taskset testing

--- OS/studios/studio10/race.c
+++ OS/studios/studio10/race.c
@@ -9,7 +9,7 @@
 #include <signal.h> //For signal()
 int race = 0;
 
-void* adder(int race) {
+void* adder(void *args) {
     int i = 0;
     for(; i<2000000; i++) {
         race++;
@@ -17,7 +17,7 @@ void* adder(int race) {
     return NULL;
 }
 
-void* subtractor(int race) {
+void* subtractor(void *args) {
     int i = 0;
     for(; i<2000000; i++) {
         race--;
@@ -30,31 +30,23 @@ int main( int argc, char* argv[] ){
     pthread_t adder_thread;
     pthread_t subtractor_thread;
     
-    printf("First check\n");
-    
-    int create_add_ret = pthread_create(&adder_thread, NULL, adder(race), NULL);
+    int create_add_ret = pthread_create(&adder_thread, NULL, adder, NULL);
     if(create_add_ret != 0){
         printf("Error creating thread. Error #%d. Exiting.", create_add_ret);
         exit(-1);
     }
     
-    printf("Second check\n");
-    
-    int create_sub_ret = pthread_create(&subtractor_thread, NULL, subtractor(race), NULL);
+    int create_sub_ret = pthread_create(&subtractor_thread, NULL, subtractor, NULL);
     if(create_sub_ret != 0){
         printf("Error creating thread. Error #%d. Exiting.", create_sub_ret);
         exit(-1);
     }
-    
-    printf("Third check\n");
     
     int join_add_ret = pthread_join(adder_thread, NULL);
     if(join_add_ret != 0){
         printf("Error joining thread. Error #%d. Exiting.", join_add_ret);
         exit(-1);
     }
-    
-    printf("Fourth check\n");;
     
     int join_sub_ret = pthread_join(subtractor_thread, NULL);
     if(join_sub_ret != 0){
