7b04452da4576a888afc175707170a5081b01233
gannonmg <gannonm3211@gmail.com>
2017-11-08 17:45:23 -0600

    helper functions started

--- OS/labs/lab4/mymalloc.c
+++ OS/labs/lab4/mymalloc.c
@@ -11,145 +11,120 @@
 #include <signal.h> //For signal()
 #include <errno.h> //For perror & errno
 
+//extern void* mymalloc(size_t size);
+//extern void myfree(void *ptr);
+
+#define array_size 500
+#define short_array 250
+
+/*
 // memory block structure
 struct memory_block {
     size_t size;
-    struct memory_block* next_block;
-    struct memory_block* prev_block;
+    int free; // 1 to indicate free, 0 to indicate not free
 };
+*/
 
 // Create linked list structure
 struct Node {
-    struct memory_block data;
+    size_t size;
+    int free; // 1 to indicate free, 0 to indicate not free
     struct Node* next;
-    struct Node* prev;
 };
+
+#define NODE_SIZE sizeof(struct Node)
 
 struct Node* head; // global variable - pointer to head node.
 
-//Creates a new Node and returns pointer to it.
-struct Node* GetNewNode(int x) {
-    struct Node* newNode
-    = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = x;
-    newNode->prev = NULL;
-    newNode->next = NULL;
-    return newNode;
-}
-
-//Inserts a Node at head of doubly linked list
-void InsertAtHead(int x) {
-    struct Node* newNode = GetNewNode(x);
-    if(head == NULL) {
-        head = newNode;
-        return;
-    }
-    head->prev = newNode;
-    newNode->next = head;
-    head = newNode;
-}
-
-//Inserts a Node at tail of Doubly linked list
-void InsertAtTail(int x) {
-    struct Node* temp = head;
-    struct Node* newNode = GetNewNode(x);
-    if(head == NULL) {
-        head = newNode;
-        return;
-    }
-    while(temp->next != NULL) temp = temp->next; // Go To last Node
-    temp->next = newNode;
-    newNode->prev = temp;
-}
-
-//Prints all the elements in linked list in forward traversal order
-void Print() {
-    struct Node* temp = head;
-    printf("Forward: ");
-    while(temp != NULL) {
-        printf("%d ",temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
-//Prints all elements in linked list in reverse traversal order.
-void ReversePrint() {
-    struct Node* temp = head;
-    if(temp == NULL) return; // empty list, exit
-    // Going to last Node
-    while(temp->next != NULL) {
-        temp = temp->next;
-    }
-    // Traversing backward using prev pointer
-    printf("Reverse: ");
-    while(temp != NULL) {
-        printf("%d ",temp->data);
-        temp = temp->prev;
-    }
-    printf("\n");
-}
-
 int main () {
-    char *str;
-
-    /* Initial memory allocation */
-    str = (char *) malloc(15);
-    strcpy(str, "tutorialspoint");
-    printf("String = %s,  Address = %u\n", str, str);
-
-    /* Reallocating memory */
-    str = (char *) realloc(str, 25);
-    strcat(str, ".com");
-    printf("String = %s,  Address = %u\n", str, str);
-
-    free(str);
-
-    return(0);
+    
+    //Create and destroy two arrays twice
+    int *array = (int*) mymalloc( sizeof(int) * array_size);
+    int *array2 = (int*) mymalloc( sizeof(int) * array_size);
+    
+    int i;
+    for(i =0 ; i< array_size; i++) {
+        array[i] = i;
+    }
+    
+    myfree(array);
+    myfree(array2);
+    
+    return 0;
 }
 
 void *mymalloc(size_t size) {
-  int page_size = sysconf(_SC_PAGESIZE);
+    
+    int page_size = sysconf(_SC_PAGESIZE);
+    
+    Node* ptr = head;
+    
+    //check if there is a free block in list of free blocks
+    if(head->next != NULL){ //there is a block in list of free blocks
+        
+        Node *pointer = (Node*) sbrk(sizeof(Node));
+        
+        pointer -> address = allocation_ptr;
+        pointer -> size = size;
+        pointer -> flag = 0;
+        pointer -> next = NULL;
+        
+        
+        head.next  = pointer;
+        
+        int bigenough = 0;
+        
+        struct Node *current = head;
+        
+        //search list for big enough block
+        struct Node *free_node = first_free(current);
+        
+        
+    }
 
-  Node* ptr = head;
+    printf("Malloc called with size %d, returning pointer to %p\n", size, ret);
+    
+    return ret;
+    
+}
 
-  //check if there is a free block in list of free blocks
-  if(head->next != NULL){ //there is a block in list of free blocks
-    int bigenough = 0;
+//Helper functions for malloc
 
-    //search list for big enough block
-    while(bigenough == 0){
-	     //check if block is big enough
-       if(head.data.size >= size){  //size of block is big enough, use it.
+//Finding a the first fitting free node
+struct Node* first_free(size_t size) {
+    struct Node *current = head;
+	while((current->next != NULL) && !((current->free == 1) && (current->size >= size))){
+        current = current->next;
+	}
+	return current;
+}
 
-       }else{
-         ptr = ptr->next;
-       }
-     }
-   }
-
+//Create a new node/memory block
+struct Node *new_node(size_t size) {
+    
     //Get memory chunk from OS
     void* ret = sbrk(size);
-
-    struct memory_block block;
-
+    struct Node *new_node;
+    new_node = ret;
+    
+    
     //Check for success, else return NULL
     if( ret == (void*)-1 ){
         printf("Error calling sbrk!\n");
         errno = ENOMEM;
         return NULL;
     }
-
-    printf("Malloc called with size %d, returning pointer to %p\n", size, ret);
-
-    return ret;
-
+    
 }
+    
 
 /*
  void myfree( void *ptr );
-
  void *mycalloc( size_t num_of_elts, size_t elt_size ) {;
-
  void *myrealloc( void *pointer, size_t size);
  */
+
+
+
+
+
