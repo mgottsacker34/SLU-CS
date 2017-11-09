cc65b41624e584fd3eed6b8a1d6b9cba1648bbe6
mgottsacker34 <mgottsacker34@gmail.com>
2017-11-09 17:43:02 -0600

    mymalloc test

--- OS/labs/lab4/mymalloc.c
+++ OS/labs/lab4/mymalloc.c
@@ -42,45 +42,45 @@ struct Node* first_free(size_t size) {
 
 //Create a new node/memory block
 struct Node *new_node(size_t size) {
-    
+
     printf("starting new node function\n");
-    
+
     //Get memory chunk from OS
     struct Node *new_node;
     new_node = sbrk(0);
     void* ret = sbrk(size  + NODE_SIZE);
     new_node = ret;
-    
+
     //Check for success, else return NULL
     if( ret == (void*)-1 ){
         printf("Error calling sbrk!\n");
         errno = ENOMEM;
         return NULL;
     }
-    
+
     new_node->size = size;
     new_node->next = NULL;
     new_node->free = 0;
-    
+
     printf("new node attributes assigned\n");
-    
+
     if (tail) {
         tail->next = new_node;
     }
-    
+
     printf("Creating head\n");
     tail = new_node;
-    
+
     printf("new node set to tail\n");
-    
+
     return tail;
 }
 
 
 void *mymalloc(size_t size) {
-    
+
     struct Node *pointer;
-    
+
     if (!head) {                        // first call
         printf("Creating head\n");
         pointer = new_node(size);
@@ -95,9 +95,9 @@ void *mymalloc(size_t size) {
             pointer->free = 0;
         }
     }
-    
+
     return pointer + 1;
-    
+
 }
 
 
@@ -109,22 +109,23 @@ void *mymalloc(size_t size) {
  */
 
 int main (int argc, const char *argv[]) {
-    
+
     //Create and destroy two arrays twice
     int *array = (int*) mymalloc(sizeof(int) * array_size);
     int *array2 = (int*) mymalloc(sizeof(int) * array_size);
-    
+
     int i;
     for(i =0 ; i< array_size; i++) {
         array[i] = i;
     }
-    
+
+    i =0;
+    for(;i<array_size; i++){
+      printf("%d", array[i]);
+    }
+
     //myfree(array);
     //myfree(array2);
-    
+
     return 0;
 }
-
-
-
-
