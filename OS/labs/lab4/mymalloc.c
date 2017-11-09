9d221e6ac5e05eb6c7d178a45f09aab27642f2e5
gannonmg <gannonm3211@gmail.com>
2017-11-09 17:34:42 -0600

    love segfaults

--- OS/labs/lab4/mymalloc.c
+++ OS/labs/lab4/mymalloc.c
@@ -26,43 +26,8 @@ struct Node {
 
 #define NODE_SIZE sizeof(struct Node)
 
-struct Node* head;          // global variable - pointer to head node.
+struct Node* head;
 struct Node* tail;
-
-int main (int argc, const char *argv[]) {
-    
-    char *str;
-    
-    /* Initial memory allocation */
-    str = (char *) mymalloc(15);
-    strcpy(str, "tutorialspoint");
-    printf("String = %s,  Address = %u\n", str, str);
-    
-    /* Reallocating memory */
-    //str = (char *) realloc(str, 25);
-    //strcat(str, ".com");
-    //printf("String = %s,  Address = %u\n", str, str);
-    
-    //free(str);
-    
-    return(0);
-    
-    /*
-    //Create and destroy two arrays twice
-    int *array = (int*) mymalloc(sizeof(int) * array_size);
-    int *array2 = (int*) mymalloc(sizeof(int) * array_size);
-    
-    int i;
-    for(i =0 ; i< array_size; i++) {
-        array[i] = i;
-    }
-    
-    //myfree(array);
-    //myfree(array2);
-    
-    return 0;
-    */
-}
 
 //Helper functions for malloc
 
@@ -78,9 +43,12 @@ struct Node* first_free(size_t size) {
 //Create a new node/memory block
 struct Node *new_node(size_t size) {
     
+    printf("starting new node function\n");
+    
     //Get memory chunk from OS
-    void* ret = sbrk(size  + NODE_SIZE);
     struct Node *new_node;
+    new_node = sbrk(0);
+    void* ret = sbrk(size  + NODE_SIZE);
     new_node = ret;
     
     //Check for success, else return NULL
@@ -94,8 +62,14 @@ struct Node *new_node(size_t size) {
     new_node->next = NULL;
     new_node->free = 0;
     
+    printf("new node attributes assigned\n");
+    
     tail->next = new_node;
+    
+    printf("Creating head\n");
     tail = new_node;
+    
+    printf("new node set to tail\n");
     
     return tail;
 }
@@ -105,8 +79,10 @@ void *mymalloc(size_t size) {
     
     struct Node *pointer;
     
-    if (!head) {        // first call
+    if (!head) {                        // first call
+        printf("Creating head\n");
         pointer = new_node(size);
+        printf("Head succesfully created\n");
         head = pointer;
         tail = head;
     } else {
@@ -120,35 +96,6 @@ void *mymalloc(size_t size) {
     
     return pointer + 1;
     
-    /*
-    int page_size = sysconf(_SC_PAGESIZE);
-    
-    //check if there is a free block in list of free blocks
-    if(head->next != NULL){ //there is a block in list of free blocks
-        
-        Node *pointer = (Node*) sbrk(sizeof(Node));
-        
-        pointer -> address = allocation_ptr;
-        pointer -> size = size;
-        pointer -> flag = 0;
-        pointer -> next = NULL;
-        
-        head->next  = pointer;
-        
-        int bigenough = 0;
-        
-        struct Node *current = head;
-        
-        //search list for big enough block
-        struct Node *free_node = first_free(current);
-        
-        
-    } else {
-
-    printf("Malloc called with size %d, returning pointer to %p\n", size, ret);
-    
-    return ret;
-    */
 }
 
 
@@ -159,6 +106,22 @@ void *mymalloc(size_t size) {
  void *myrealloc( void *pointer, size_t size);
  */
 
+int main (int argc, const char *argv[]) {
+    
+    //Create and destroy two arrays twice
+    int *array = (int*) mymalloc(sizeof(int) * array_size);
+    int *array2 = (int*) mymalloc(sizeof(int) * array_size);
+    
+    int i;
+    for(i =0 ; i< array_size; i++) {
+        array[i] = i;
+    }
+    
+    //myfree(array);
+    //myfree(array2);
+    
+    return 0;
+}
 
 
 
